cd2633b621446641e20bbe022a31ff8233ab4b9a
grez72 <grez72@gmail.com>
2022-10-14 10:00:12 -0400

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -6,5 +6,5 @@ import models
 
 dependencies = ['torch', 'torchvision']
 
-def slip_vits16_clip_25ep_yfcc15M(pretrained=True, **kwargs):
-  return torch.hub.load("harvard-visionlab/slip", "slip_vits16_clip_25ep_yfcc15M")
+def slip_vits16_clip_25ep_yfcc15M(**kwargs):
+  return torch.hub.load("harvard-visionlab/slip", "slip_vits16_clip_25ep_yfcc15M", **kwargs)
